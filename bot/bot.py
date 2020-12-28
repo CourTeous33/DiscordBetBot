915ed61ca6d20ed6e638f6b511901beada4eed18
shirley zhang <shirley757363157@gmail.com>
2020-12-27 19:57:22 -0800

    db

--- bot/bot.py
+++ bot/bot.py
@@ -1,6 +1,7 @@
 # bot.py
 import os
 import random
+import sqlite3
 
 import discord
 from dotenv import load_dotenv
@@ -36,7 +37,7 @@ async def on_message(message):
         'test': m_test,
         'facts': m_facts,
     }
-    
+
     args = message.content.split(' ')
 
     response = switcher.get(args[0][1:], lambda: 'Invalid')(args[1:])
@@ -44,18 +45,87 @@ async def on_message(message):
     if response == 'Invalid':
         print('Invalid Command: ' + message.content + '\nwith details: ' + str(message))
         return
-        
+
     await message.channel.send(response)
-    
+
 
 def m_test(args):
     return 'You got me :)'
 
-def m_facts(args):
+def _all_facts(args):
     facts_list = [
-        '俞勇哲是狗',
-        '没有人🙌比雪梨🙌更懂🙌k头',
+        # '俞勇哲是狗',
+        # '没有人🙌比雪梨🙌更懂🙌k头',
     ]
-    return random.choice(facts_list)
+    conn = sqlite3.connect('dc.db')
+    c = conn.cursor()
+    qry = 'SELECT DISTINCT text FROM facts'
+    c.execute(qry)
+    for row in c:
+        facts_list.append(row[0])
+    conn.commit()
+    conn.close()
+    return facts_list
+
+def _random_facts(args):
+    facts_list = [
+        # '俞勇哲是狗',
+        # '没有人🙌比雪梨🙌更懂🙌k头',
+    ]
+    conn = sqlite3.connect('dc.db')
+    c = conn.cursor()
+    qry = 'SELECT DISTINCT text FROM facts ORDER BY RANDOM() LIMIT 1'
+    c.execute(qry)
+    facts = c.fetchone()[0]
+    conn.commit()
+    conn.close()
+    return facts
+
+def m_addfacts(fact):
+    conn = sqlite3.connect('dc.db')
+    c = conn.cursor()
+    qry = 'INSERT INTO facts VALUES(?)'
+    c.execute(qry, fact)
+    conn.commit()
+    conn.close()
+
+def _remove_facts(fact):
+    conn = sqlite3.connect('dc.db')
+    c = conn.cursor()
+    qry = 'DELETE from facts WHERE text = ?'
+    c.execute(qry, fact)
+    conn.commit()
+    conn.close()
+
+def m_checkcredit(user):
+    conn = sqlite3.connect('dc.db')
+    c = conn.cursor()
+    qry = 'SELECT credit FROM users WHERE id = ?'
+    c.execute(qry, user)
+    credit = c.fetchone()[0]
+    conn.commit()
+    conn.close()
+    return credit
+
+def _change_credit(user, amount):
+    credit = m_checkcredit(user)
+    credit = credit + amount
+    qry = 'UPDATE users SET credit = ? WHERE id = ?'
+    conn = sqlite3.connect('dc.db')
+    c = conn.cursor()
+    data = [credit, user]
+    c.execute(qry, data)
+    conn.commit()
+    conn.close()
+
+def _set_credit(user, amount):
+    qry = 'UPDATE users SET credit = ? WHERE id = ?'
+    conn = sqlite3.connect('dc.db')
+    c = conn.cursor()
+    data = [amount, user]
+    c.execute(qry, data)
+    conn.commit()
+    conn.close()
+
 
 client.run(TOKEN)
